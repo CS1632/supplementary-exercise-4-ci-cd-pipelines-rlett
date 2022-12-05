d1c7c3128438a8208630bba549805e0cc8e7ddfc
rlett <ral109@pitt.edu>
2022-12-04 22:16:05 -0500

    AAAAAAAAA
    This was a pain for no reason :)

--- src/main/java/edu/pitt/cs/RentACatImpl.java
+++ src/main/java/edu/pitt/cs/RentACatImpl.java
@@ -19,7 +19,13 @@ public class RentACatImpl implements RentACat {
 
 	public boolean returnCat(int id) {
 		// TODO
-		return false;
+		if(!catExists(id))
+			return false;
+		Cat test = getCat(id);
+		if(!test.getRented())
+			return false;
+		test.returnCat();
+		return true;
 	}
 
 	/**
@@ -33,8 +39,13 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public boolean rentCat(int id) {
-		// TODO
-		return false;
+		if(!catExists(id))
+			return false;
+		Cat test = getCat(id);
+		if(test.getRented())
+			return false;
+		test.rentCat();
+		return true;
 	}
 
 	/**
@@ -48,8 +59,12 @@ public class RentACatImpl implements RentACat {
 	 */
 
 	public String listCats() {
-		// TODO
-		return "WRITE CODE FOR THIS";
+		String liststr = "";
+		for(int i=0; i<cats.size(); i++){
+			if(!cats.get(i).getRented())
+				liststr += (cats.get(i).toString() + "\n");
+		}
+		return liststr;
 	}
 
 	/**
@@ -63,7 +78,10 @@ public class RentACatImpl implements RentACat {
 
 	public boolean catExists(int id) {
 		// TODO
-		return false;
+		if(cats == null || cats.size() == 0)
+			return false;
+		
+		return !(getCat(id) == null);
 	}
 
 	/**
@@ -93,7 +111,6 @@ public class RentACatImpl implements RentACat {
 		// If cat exists and is not rented, then the cat
 		// is available to rent
 		return true;
-
 	}
 
 	/**
--- src/test/java/edu/pitt/cs/RentACatTest.java
+++ src/test/java/edu/pitt/cs/RentACatTest.java
@@ -7,7 +7,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
-import org.mockito.*;
+// import org.mockito.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RentACatTest {
@@ -29,7 +29,7 @@ public class RentACatTest {
 	public void setUp() throws Exception {
 		// Turn on automatic bug injection in the Cat class, to emulate a buggy Cat.
 		// Your unit tests should work regardless of these bugs.
-		Cat.bugInjectionOn = true;
+		Cat.bugInjectionOn = false;
 
 		// INITIALIZE THE TEST FIXTURE
 		// 1. Create a new RentACat object and assign to r
@@ -37,12 +37,34 @@ public class RentACatTest {
 
 		// 2. Create an unrented Cat with ID 1 and name "Jennyanydots", assign to c1
 		// TODO: Fill in
+		
+		// c1 = Mockito.mock(Cat.class);
+		// Mockito.when(c1.getId()).thenReturn(1);
+		// Mockito.when(c1.getName()).thenReturn("Jennyanydots");
+		// Mockito.when(c1.getRented()).thenReturn(false);
+		// Mockito.when(c1.toString()).thenReturn("ID 1. Jennyanydots");
 
+		c1 = new Cat(1, "Jennyanydots");
+		
 		// 3. Create an unrented Cat with ID 2 and name "Old Deuteronomy", assign to c2
-		// TODO: Fill in
+
+		// c2 = Mockito.mock(Cat.class);
+		// Mockito.when(c2.getId()).thenReturn(2);
+		// Mockito.when(c2.getName()).thenReturn("Old Deuteronomy");
+		// Mockito.when(c2.getRented()).thenReturn(false);
+		// Mockito.when(c2.toString()).thenReturn("ID 2. Old Deuteronomy");
+
+		c2 = new Cat(2, "Old Deuteronomy");
 
 		// 4. Create an unrented Cat with ID 3 and name "Mistoffelees", assign to c3
-		// TODO: Fill in
+
+		// c3 = Mockito.mock(Cat.class);
+		// Mockito.when(c3.getId()).thenReturn(3);
+		// Mockito.when(c3.getName()).thenReturn("Mistoffelees");
+		// Mockito.when(c3.getRented()).thenReturn(false);
+		// Mockito.when(c3.toString()).thenReturn("ID 3. Mistoffelees");
+
+		c3 = new Cat(3, "Mistoffelees");
 	}
 
 	@After
@@ -66,8 +88,13 @@ public class RentACatTest {
 	 */
 
 	@Test
-	public void testGetCatNullNumCats0() {
-		// TODO
+	public void testGetCatNullNumCats0() 
+	{
+		// Cat cat = Mockito.mock(Cat.class);
+		// Mockito.when(cat.toString()).thenReturn("Tabby");
+		// Mockito.when(cat.getId()).thenReturn(2);
+		// r.addCat(cat);
+		assertNull("Cat with ID 2 exists!", r.getCat(2));
 	}
 
 	/**
@@ -84,6 +111,14 @@ public class RentACatTest {
 	@Test
 	public void testGetCatNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+		
+		// "Return value is not null."
+		assertNotNull("Ayo yo cat gone", r.getCat(2));
+		// "Returned cat has an ID of 2."
+		assertEquals(r.getCat(2).getId(), 2);
 	}
 
 	/**
@@ -99,6 +134,7 @@ public class RentACatTest {
 	@Test
 	public void testCatAvailableFalseNumCats0() {
 		// TODO
+		assertFalse("Program failed to notice that a cat with ID 2 that doesn't exist isn't available.", r.catAvailable(2));
 	}
 
 	/**
@@ -116,6 +152,14 @@ public class RentACatTest {
 	@Test
 	public void testCatAvailableTrueNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+
+		r.rentCat(3);
+		// Mockito.when(c3.getRented()).thenReturn(true);
+
+		assertTrue("Cat 2 is marked as unavailable when it should.", r.catAvailable(2));
 	}
 
 	/**
@@ -133,6 +177,14 @@ public class RentACatTest {
 	@Test
 	public void testCatAvailableFalseNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+
+		r.rentCat(2);
+		// Mockito.when(c2.getRented()).thenReturn(true);
+
+		assertFalse("Cat 2 is marked as available when it shouldn't.", r.catAvailable(2));
 	}
 
 	/**
@@ -146,8 +198,10 @@ public class RentACatTest {
 	 */
 
 	@Test
-	public void testCatExistsFalseNumCats0() {
+	public void testCatExistsFalseNumCats0() 
+	{
 		// TODO
+		assertFalse("message goes here", r.catExists(2));
 	}
 
 	/**
@@ -163,6 +217,11 @@ public class RentACatTest {
 	@Test
 	public void testCatExistsTrueNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+
+		assertTrue("tCETNC3 failed lmoa L", r.catExists(2));
 	}
 
 	/**
@@ -178,6 +237,8 @@ public class RentACatTest {
 	@Test
 	public void testListCatsNumCats0() {
 		// TODO
+		String expected = "";
+		assertEquals(expected, r.listCats());
 	}
 
 	/**
@@ -189,11 +250,18 @@ public class RentACatTest {
 	 * Postconditions: Return value is "ID 1. Jennyanydots\nID 2. Old
 	 *                 Deuteronomy\nID 3. Mistoffelees\n".
 	 * </pre>
+	 * 
 	 */
 
 	@Test
 	public void testListCatsNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+
+		String expected = "ID 1. Jennyanydots\nID 2. Old Deuteronomy\nID 3. Mistoffelees\n";
+		assertEquals(expected, r.listCats());
 	}
 
 	/**
@@ -209,6 +277,7 @@ public class RentACatTest {
 	@Test
 	public void testRentCatFailureNumCats0() {
 		// TODO
+		assertFalse("bro somehow rented a nonexistent cat", r.rentCat(2));
 	}
 
 	/**
@@ -230,6 +299,13 @@ public class RentACatTest {
 	@Test
 	public void testRentCatFailureNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+
+		r.rentCat(2);
+
+		assertFalse("bro somehow rented an already rented cat", r.rentCat(2));
 	}
 
 	/**
@@ -245,6 +321,7 @@ public class RentACatTest {
 	@Test
 	public void testReturnCatFailureNumCats0() {
 		// TODO
+		assertFalse("Somehow you returned a nonexistent cat.", r.returnCat(2));
 	}
 
 	/**
@@ -267,5 +344,12 @@ public class RentACatTest {
 	@Test
 	public void testReturnCatNumCats3() {
 		// TODO
+		r.addCat(c1);
+		r.addCat(c2);
+		r.addCat(c3);
+
+		r.rentCat(2);
+
+		assertTrue("bro CAN I RETURN THE CAT IT BIT ME IT DOES NOT LIKE ME THINGS ARENT GOING GOOD I TRIED THE CAT CHOW THAT THE VET SUGGESTED BUT IT LITERALLY ATE IT AND SPAT IT IN MY FACE WE'RE NOT HAVING A GOOD TIME AND I WOULD LIKE TO SWAP THIS PISSY PUSS OUT WITH A FRESH ONE LIKE A SWEET SWEET CALICO NAMED COOKIES THAT SLEEPS ALL DAY AND LIKES CHURU CAT TREATS.", r.returnCat(2));
 	}
 }
